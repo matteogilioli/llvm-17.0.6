1425d920c5071c4188a246f759669b73a52c0035
Matteo Gilioli <55749040+matteogilioli@users.noreply.github.com>
2024-05-10 10:24:38 +0200

    1° Assignment

--- src/llvm/lib/Transforms/Utils/LocalOpts.cpp
+++ src/llvm/lib/Transforms/Utils/LocalOpts.cpp
@@ -7,10 +7,41 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Utils/LocalOpts.h"
-#include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instructions.h"
+#include <llvm/IR/Constants.h>
 
 using namespace llvm;
+
+bool algebraicIdentity(Instruction &inst) {
+  bool transformed = false;
+  if (inst.getOpcode() == Instruction::Add) {
+    // Algebric identity: x + 0 = x
+    for (int op = 0; op < 2; op++) {
+      if (ConstantInt *c = dyn_cast<ConstantInt>(inst.getOperand(op))) {
+        if (c->getValue().isZero()) {
+          inst.replaceAllUsesWith(inst.getOperand(op == 1 ? 0 : 1));
+          outs() << "Sostituzione di " << inst << " con "
+                 << *inst.getOperand(op == 1 ? 0 : 1) << '\n';
+          transformed = true;
+        }
+      }
+    }
+  } else if (inst.getOpcode() == Instruction::Mul) {
+    // Algebric identity: x * 1 = x
+    for (int op = 0; op < 2; op++) {
+      if (ConstantInt *c = dyn_cast<ConstantInt>(inst.getOperand(op))) {
+        if (c->getValue().isOne()) {
+          inst.replaceAllUsesWith(inst.getOperand(op == 1 ? 0 : 1));
+          outs() << "Sostituzione di " << inst << " con "
+                 << *inst.getOperand(op == 1 ? 0 : 1) << '\n';
+          transformed = true;
+        }
+      }
+    }
+  }
+  return transformed;
+}
 
 bool strengthReduction(Instruction &inst) {
   bool transformed = false;
@@ -29,17 +60,128 @@ bool strengthReduction(Instruction &inst) {
           inst.replaceAllUsesWith(shl);
           outs() << "Sostituzione di " << inst << " con " << *shl << '\n';
           transformed = true;
+        } else if (((value - 1) & (value - 1 - 1)) == 0) {
+          // Potenza di due + 1
+          Instruction *shl = BinaryOperator::Create(
+              Instruction::Shl, inst.getOperand(op == 1 ? 0 : 1),
+              ConstantInt::get(c->getType(), APInt(32, value - 1).logBase2()));
+          Instruction *add = BinaryOperator::Create(
+              Instruction::Add, shl, inst.getOperand(op == 1 ? 0 : 1));
+          shl->insertAfter(&inst);
+          add->insertAfter(shl);
+          inst.replaceAllUsesWith(add);
+          outs() << "Sostituzione di " << inst << " con " << *shl << " e con "
+                 << *add << '\n';
+          transformed = true;
+        } else if (((value + 1) & (value - 1 + 1)) == 0) {
+          // Potenza di due - 1
+          Instruction *shl = BinaryOperator::Create(
+              Instruction::Shl, inst.getOperand(op == 1 ? 0 : 1),
+              ConstantInt::get(c->getType(), APInt(32, value + 1).logBase2()));
+          Instruction *sub = BinaryOperator::Create(
+              Instruction::Sub, shl, inst.getOperand(op == 1 ? 0 : 1));
+          shl->insertAfter(&inst);
+          sub->insertAfter(shl);
+          inst.replaceAllUsesWith(sub);
+          outs() << "Sostituzione di " << inst << " con " << *shl << " e con "
+                 << *sub << '\n';
+          transformed = true;
         }
+      }
+    }
+  } else if (inst.getOpcode() == Instruction::SDiv) {
+    // Divisione per potenza di 2
+    if (ConstantInt *c = dyn_cast<ConstantInt>(inst.getOperand(1))) {
+      if (c->getValue().isPowerOf2()) {
+        // crea un instruzione shl che fa lo shift del logaritmo del valore
+        // dell'operando costante
+        Instruction *lshr = BinaryOperator::Create(
+            Instruction::LShr, inst.getOperand(0),
+            ConstantInt::get(c->getType(), c->getValue().logBase2()));
+        // inserimento dell'istruzione lshr dopo la mul e aggiornamento
+        // utilizzi
+        lshr->insertAfter(&inst);
+        inst.replaceAllUsesWith(lshr);
+        outs() << "Sostituzione di " << inst << " con " << *lshr << '\n';
+        transformed = true;
       }
     }
   }
   return transformed;
 }
 
+bool multiInstructionOptimization(Instruction &inst1) {
+  BasicBlock &B = *inst1.getParent();
+  BasicBlock::iterator it = inst1.getIterator();
+  BasicBlock::iterator nextIt = ++it;
+
+  if (nextIt == B.end())
+    return false;
+
+  Instruction &inst2 = *nextIt;
+
+  bool transformed = false;
+
+  if (inst1.getOpcode() == Instruction::Add &&
+      inst2.getOpcode() == Instruction::Sub) {
+    ConstantInt *c0inst1 = dyn_cast<ConstantInt>(inst1.getOperand(0));
+    ConstantInt *c1inst1 = dyn_cast<ConstantInt>(inst1.getOperand(1));
+    ConstantInt *c0inst2 = dyn_cast<ConstantInt>(inst2.getOperand(0));
+    ConstantInt *c1inst2 = dyn_cast<ConstantInt>(inst2.getOperand(1));
+
+    // a = b + 1, c = a - 1 => a = b + 1, c = b
+    if ((!c0inst1 && c1inst1) && (!c0inst2 && c1inst2) &&
+        (&inst1 == inst2.getOperand(0)) &&
+        (c1inst1->getValue() == c1inst2->getValue())) {
+      inst2.replaceAllUsesWith(inst1.getOperand(0));
+      outs() << "Sostituzione di " << inst2 << " con " << *inst1.getOperand(0)
+             << '\n';
+      transformed = true;
+      // a = 1 + b, c = a - 1 => a = 1 + b, c = b
+    } else if ((c0inst1 && !c1inst1) && (!c0inst2 && c1inst2) &&
+               (&inst1 == inst2.getOperand(0)) &&
+               (c0inst1->getValue() == c1inst2->getValue())) {
+      inst2.replaceAllUsesWith(inst1.getOperand(1));
+      outs() << "Sostituzione di " << inst2 << " con " << *inst1.getOperand(1)
+             << '\n';
+      transformed = true;
+    }
+  } else if (inst1.getOpcode() == Instruction::Sub &&
+             inst2.getOpcode() == Instruction::Add) {
+    ConstantInt *c0inst1 = dyn_cast<ConstantInt>(inst1.getOperand(0));
+    ConstantInt *c1inst1 = dyn_cast<ConstantInt>(inst1.getOperand(1));
+    ConstantInt *c0inst2 = dyn_cast<ConstantInt>(inst2.getOperand(0));
+    ConstantInt *c1inst2 = dyn_cast<ConstantInt>(inst2.getOperand(1));
+    // a = b - 1, c = a + 1 => a = b - 1, c = b
+    if ((!c0inst1 && c1inst1) && (!c0inst2 && c1inst2) &&
+        (&inst1 == inst2.getOperand(0)) &&
+        (c1inst1->getValue() == c1inst2->getValue())) {
+      inst2.replaceAllUsesWith(inst1.getOperand(0));
+      outs() << "Sostituzione di " << inst2 << " con " << *inst1.getOperand(0)
+             << '\n';
+      transformed = true;
+      // a = b - 1, c = 1 + a => a = b - 1, c = b
+    } else if ((!c0inst1 && c1inst1) && (c0inst2 && !c1inst2) &&
+               (&inst1 == inst2.getOperand(1)) &&
+               (c1inst1->getValue() == c0inst2->getValue())) {
+      inst2.replaceAllUsesWith(inst1.getOperand(0));
+      outs() << "Sostituzione di " << inst2 << " con " << *inst1.getOperand(0)
+             << '\n';
+      transformed = true;
+    }
+  }
+
+  return transformed;
+}
+
 bool runOnBasicBlock(BasicBlock &B) {
   bool isModified = false;
   for (Instruction &inst : B) {
+    if (algebraicIdentity(inst))
+      isModified = true;
     if (strengthReduction(inst))
+      isModified = true;
+    if (multiInstructionOptimization(inst))
       isModified = true;
   }
   return isModified;
@@ -56,7 +198,6 @@ bool runOnFunction(Function &F) {
 
   return Transformed;
 }
-
 
 PreservedAnalyses LocalOpts::run(Module &M, ModuleAnalysisManager &AM) {
   for (auto Fiter = M.begin(); Fiter != M.end(); ++Fiter)
